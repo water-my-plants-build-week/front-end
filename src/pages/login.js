f3078cf71995efd695ce44d9c6560c64639fe9f2
Matt Hagner <web@matthagner.com>
2019-07-25 20:32:18 -0500

    Add login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import RegisterForm from "../components/register-form";
+import LoginForm from "../components/login-form";
 
 const Login = () => {
   return (
     <>
       <h1>Login Page</h1>
       <RegisterForm />
+      <br />
+      <LoginForm />
     </>
   );
 };
