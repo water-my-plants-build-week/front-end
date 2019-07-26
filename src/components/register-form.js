de51f102a13a815b9f94f193854979e933d48dad
Matt Hagner <web@matthagner.com>
2019-07-25 19:23:14 -0500

    Add ability to register user

---
+++ src/components/register-form.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { connect } from "react-redux";
+import { registerUser } from "../actions";
+
+class RegisterForm extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    phoneNumber: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { username, password, phoneNumber } = this.state;
+
+    this.props
+      .registerUser(username, password, phoneNumber)
+      .then(() => {
+        console.log("success");
+      })
+      .catch(() => {
+        console.log("failure :(");
+      });
+  };
+
+  render() {
+    const { username, password, phoneNumber } = this.state;
+    const { handleSubmit, handleChange } = this;
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <input
+          id="phoneNumber"
+          type="text"
+          name="phoneNumber"
+          value={phoneNumber}
+          onChange={handleChange}
+        />
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
+}
+
+const mapStateToProps = ({ isLoading, errorMessage }) => {
+  return {
+    isLoading,
+    errorMessage
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(RegisterForm);
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import RegisterForm from "../components/register-form";
+
 const Login = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <>
+      <h1>Login Page</h1>
+      <RegisterForm />
+    </>
+  );
 };
 
 export default Login;
