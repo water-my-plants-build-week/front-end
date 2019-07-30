db051269cfa0792dfcae14cf7b4da8aecf1ddc93
Matt Hagner <web@matthagner.com>
2019-07-30 10:57:06 -0500

    Add tests for authorization actions
    - Add tests for registering and logging in a user
    - Refactor authorzation actions into their own module

---
+++ src/actions/authorization.js
@@ -0,0 +1,138 @@
+import axios from "axios";
+import { BASE_URL } from "./index";
+
+export const REGISTER_START = "REGISTER_START";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAILURE = "REGISTER_FAILURE";
+
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+export const registerUser = ({
+  username,
+  password,
+  phoneNumber,
+  timezone,
+  useTwilio = true
+} = {}) => async dispatch => {
+  dispatch({ type: REGISTER_START });
+
+  return axios
+    .post(`${BASE_URL}/auth/register`, {
+      username,
+      password,
+      phoneNumber,
+      timezone,
+      useTwilio
+    })
+    .then(() => {
+      /*
+       * On a successful registration we are going to auto-login the user
+       * so we are not concerned with the data that is returned from the server
+       * on a successful registration.
+       *
+       * */
+
+      dispatch({ type: REGISTER_SUCCESS });
+    })
+    .catch(err => {
+      let message;
+
+      if (err.response) {
+        if (err.response.status === 400) {
+          message = err.response.data.message;
+        } else if (
+          err.response.status === 500 &&
+          typeof err.response.data !== "string"
+        ) {
+          message = "Username is already taken";
+        }
+      }
+
+      if (!message) {
+        message = "Please provide a username and a password";
+      }
+
+      dispatch({ type: REGISTER_FAILURE, payload: { message } });
+
+      /*
+       * We want to use the promise returned from this action to determine
+       * whether we should redirect the user, so in the case of an error we
+       * should throw an error so the front end can catch. The error message
+       * isn't as important because we are putting an error message into state
+       * that we can display for the user.
+       * */
+
+      throw new Error(message);
+    });
+};
+
+export const login = (username, password) => async dispatch => {
+  dispatch({ type: "LOGIN_START" });
+
+  return axios
+    .post(`${BASE_URL}/auth/login`, { username, password })
+    .then(res => {
+      /*
+       * On a 200 the server returns an object with the shape:
+       *
+       * {
+       *   user: {
+       *     id: number,
+       *     username: string,
+       *     password: string, (hashed)
+       *     phoneNumber: string || null,
+       *     timezone: string || null,
+       *     useTwilio: boolean, (defaults to 0?)
+       *     avatar_id: string || null,
+       *   },
+       *   token: string,
+       * }
+       **/
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: {
+          token: res.data.token,
+          user: res.data.user
+        }
+      });
+    })
+    .catch(err => {
+      let message;
+      let status;
+      if (err.response) {
+        status = err.response.status;
+        switch (err.response.status) {
+          case 400:
+            message = "Please provide a username and a password";
+            break;
+          case 401:
+            message = "Invalid credentials";
+            break;
+          case 500:
+            message = "Something went wrong. Try again";
+            break;
+          default:
+        }
+      }
+
+      if (!message) {
+        message = "Something went wrong. Try again";
+      }
+
+      dispatch({ type: LOGIN_FAILURE, payload: { message, status } });
+
+      /*
+       * Because we want to redirect the user to a protected route after being
+       * successfully logged in, and we do so via promise chaining, we need to
+       * throw an error on a server error to prevent that from happening.
+       *
+       * The error can be caught by the front end, but we are dispatching an
+       * error with a payload anyways, so we can use that that payload to
+       * display more information to the user on the front end.
+       * */
+      throw new Error(message);
+    });
+};
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,60 +1,26 @@
-import axios from "axios";
-
 import axiosAuth from "../middleware/axios-auth";
+import {
+  REGISTER_START,
+  REGISTER_SUCCESS,
+  REGISTER_FAILURE,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  registerUser,
+  login
+} from "./authorization";
 
-const BASE_URL = "https://water-my-plants-lambda.herokuapp.com/api";
-
-export const REGISTER_START = "REGISTER_START";
-export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
-export const REGISTER_FAILURE = "REGISTER_FAILURE";
-
-export const LOGIN_START = "LOGIN_START";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const BASE_URL = "https://water-my-plants-lambda.herokuapp.com/api";
 
 export const GETTING_PLANTS = "GETTING_PLANTS";
 export const GOT_PLANTS = "GOT_PLANTS";
 export const ADDED_PLANTS = "ADDED_PLANTS";
 export const DELETED_PLANTS = "DELETED_PLANTS";
 
-export const registerUser = (
-  username,
-  password,
-  phoneNumber
-) => async dispatch => {
-  dispatch({ type: REGISTER_START });
-
-  return axios
-    .post(`${BASE_URL}/auth/register`, { username, password, phoneNumber })
-    .then(() => {
-      dispatch({ type: REGISTER_SUCCESS });
-    })
-    .catch(() => {
-      dispatch({ type: REGISTER_FAILURE, payload: "Failed to Register 😭" });
-      throw new Error("Failed to register");
-    });
-};
-
-export const login = (username, password) => async dispatch => {
-  dispatch({ type: LOGIN_START });
-
-  return axios
-    .post(`${BASE_URL}/auth/login`, { username, password })
-    .then(res => {
-      const userId = res.data.user.id;
-
-      localStorage.setItem("userId", userId);
-
-      dispatch({ type: LOGIN_SUCCESS, payload: res.data.token });
-    })
-    .catch(err => {
-      dispatch({ type: LOGIN_FAILURE, payload: err.response.data.message });
-      throw new Error("Failed to login");
-    });
-};
-
-export const getPlants = () => async dispatch => {
-  const id = localStorage.getItem("userId");
+export const getPlants = () => async (dispatch, getState) => {
+  const { user } = getState();
+  const id = user ? user.id : "will catch";
+  console.log(id);
 
   dispatch({ type: GETTING_PLANTS });
   return axiosAuth()
@@ -88,4 +54,15 @@ export const deletePlant = id => async dispatch => {
     .catch(err => {
       console.log(err);
     });
+};
+
+export {
+  REGISTER_START,
+  REGISTER_FAILURE,
+  REGISTER_SUCCESS,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  login,
+  registerUser
 };
