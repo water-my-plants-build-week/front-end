1c084dd076d40de4a99b1f51adfef2eb0f84b065
Dannyyzz <dannyzz@live.com>
2019-07-29 20:17:53 -0400

    axios auth

---
+++ src/middleware/axios-auth.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export default function axiosAuth () {
+    const token = localStorage.getItem("token")
+    return axios.create({ headers: {"Content-Type": "application/json", Authorization: `${token}`}  })
+}
